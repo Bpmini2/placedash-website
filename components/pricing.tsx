29cb16e32909006ce410b73e3b0ed564d195c3ff
Bpmini2 <bpmini2002@gmail.com>
2026-04-26 22:31:58 +1000

    Update pricing.tsx

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -113,7 +113,7 @@ export function Pricing() {
               <a
   href={
     plan.name === "Pro"
-      ? "https://buy.stripe.com/60U00kecg9tn8Yz6eY0kE00"
+      ? "https://buy.stripe.com/6oU00kecg9tn8YZ6eY0kE00"
       : "#"}
   }
   target="_blank"
