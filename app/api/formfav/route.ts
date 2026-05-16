96b16e1bb06ec4bdaf1c0f8d84a0e7d9751bbc67
Bpmini2 <bpmini2002@gmail.com>
2026-05-17 01:06:39 +1000

    Update route.ts

--- app/api/formfav/route.ts
+++ app/api/formfav/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../../lib/supabase.ts";
 
 function getMelbourneDate() {
   return new Date().toLocaleDateString("en-CA", {
